5c3ae55ad42346c729d2020240ac97fccac5577f
Marknetreba <mark.netreba@mail.ru>
2018-05-23 22:32:53 +0400

    styles v. 1.0

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './styles/App.css';
+import 'semantic-ui/dist/semantic.min.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Контактцентер</h1>
+          <i className="huge phone volume icon"></i>
+          <h1 className="App-title">Контактцентр</h1>
         </header>
           <div className="ui middle aligned center aligned grid">
               <div className="column">
-                  <h2 className="ui teal image header">
+                  <h2 className="ui black image header">
                           <div className="content">
-                              Log-in to your account
+                              Войти в личный кабинет
                           </div>
                   </h2>
                   <form className="ui large form">
@@ -22,16 +22,16 @@ class App extends Component {
                           <div className="field">
                               <div className="ui left icon input">
                                   <i className="user icon"></i>
-                                  <input type="text" name="email" placeholder="E-mail address"/>
+                                  <input type="text" name="email" placeholder="E-mail адрес"/>
                               </div>
                           </div>
                           <div className="field">
                               <div className="ui left icon input">
                                   <i className="lock icon"></i>
-                                  <input type="password" name="password" placeholder="Password"/>
+                                  <input type="password" name="password" placeholder="Пароль"/>
                               </div>
                           </div>
-                          <div className="ui fluid large teal submit button">Login</div>
+                          <div className="ui fluid large black submit button">Войти</div>
                       </div>
                   </form>
               </div>
