24f005af7a82e1a277aa950f939c9d322cb69aac
Marknetreba <mark.netreba@mail.ru>
2018-05-23 22:04:42 +0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -8,11 +8,34 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Контактцентер</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="ui middle aligned center aligned grid">
+              <div className="column">
+                  <h2 className="ui teal image header">
+                          <div className="content">
+                              Log-in to your account
+                          </div>
+                  </h2>
+                  <form className="ui large form">
+                      <div className="ui stacked segment">
+                          <div className="field">
+                              <div className="ui left icon input">
+                                  <i className="user icon"></i>
+                                  <input type="text" name="email" placeholder="E-mail address"/>
+                              </div>
+                          </div>
+                          <div className="field">
+                              <div className="ui left icon input">
+                                  <i className="lock icon"></i>
+                                  <input type="password" name="password" placeholder="Password"/>
+                              </div>
+                          </div>
+                          <div className="ui fluid large teal submit button">Login</div>
+                      </div>
+                  </form>
+              </div>
+          </div>
       </div>
     );
   }
