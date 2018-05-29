fd29db23d8325e4095015f0be9ee377b96436b93
Marknetreba <mark.netreba@mail.ru>
2018-05-29 14:29:03 +0400

    new react-semantic package added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,41 @@ import React, {Component} from 'react';
 import './styles/App.css';
 import 'semantic-ui/dist/semantic.min.css';
 import 'semantic-ui/dist/semantic.min'
+import { Button, Tab } from 'semantic-ui-react'
+
+const panes = [
+    { menuItem: 'О компании', render: () => <Tab.Pane>Описание компании</Tab.Pane> },
+    { menuItem: 'О продукте', render: () => <Tab.Pane>Описание контактцентра</Tab.Pane> },
+    { menuItem: 'Войти', render: () => <Tab.Pane>
+            <div className="ui middle aligned center aligned grid">
+            <div className="column">
+                <h2 className="ui black image header">
+                    <div className="content">
+                        Войти в личный кабинет
+                    </div>
+                </h2>
+                <form className="ui large form">
+                    <div className="ui stacked segment">
+                        <div className="field">
+                            <div className="ui left icon input">
+                                <i className="user icon"></i>
+                                <input type="text" name="email" placeholder="E-mail адрес"/>
+                            </div>
+                        </div>
+                        <div className="field">
+                            <div className="ui left icon input">
+                                <i className="lock icon"></i>
+                                <input type="password" name="password" placeholder="Пароль"/>
+                            </div>
+                        </div>
+
+                        <Button fluid primary>Войти</Button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        </Tab.Pane> },
+];
 
 class App extends Component {
   render() {
@@ -10,45 +45,8 @@ class App extends Component {
         <header className="App-header">
           <i className="huge phone volume icon"></i>
           <h1 className="App-title">Контактцентр</h1>
-            <div className="ui large three item menu black">
-                <div className="item active">
-                    О компании
-                </div>
-                <a className="item">
-                    О продукте
-                </a>
-                <div className="item">
-                    Вход
-                </div>
-            </div>
         </header>
-          <div className="ui middle aligned center aligned grid">
-              <div className="column">
-                  <h2 className="ui black image header">
-                          <div className="content">
-                              Войти в личный кабинет
-                          </div>
-                  </h2>
-                  <form className="ui large form">
-                      <div className="ui stacked segment">
-                          <div className="field">
-                              <div className="ui left icon input">
-                                  <i className="user icon"></i>
-                                  <input type="text" name="email" placeholder="E-mail адрес"/>
-                              </div>
-                          </div>
-                          <div className="field">
-                              <div className="ui left icon input">
-                                  <i className="lock icon"></i>
-                                  <input type="password" name="password" placeholder="Пароль"/>
-                              </div>
-                          </div>
-
-                          <button className="ui fluid large black submit button">Войти</button>
-                      </div>
-                  </form>
-              </div>
-          </div>
+          <Tab panes={panes}/>
       </div>
     );
   }
